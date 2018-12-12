76e96e24ac0805e9e566862699719d4377e8202a
vagrant <vagrant@kleer-csd-box>
2018-12-12 18:37:58 -0300

    Merge branch 'master' of https://github.com/schildren/csd-ahorcado
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- spec/palabra_spec.rb
+++ spec/palabra_spec.rb
@@ -19,12 +19,10 @@ describe Palabra do
   palabra.contiene("t").should == "False"
  end
 
-
  it "Al dividir la palaba Magio, se deberia mostrar un vector con 5 caracteres" do
-		palabra=Palabra.new
-		vector=palabra.dividir("Magio")
-		vector.size==5
-	end
-
+  palabra=Palabra.new
+  vector=palabra.dividir("Magio")
+  vector.size==5
+ end
 
 end
